61e16b0875832a1480f45e77255a489f711ac4cc
francesco <fviscomi@gmail.com>
2023-09-05 22:35:12 +0200

    first commit

---
+++ src/main/java/it/franz/padre/networking/App.java
@@ -0,0 +1,13 @@
+package it.franz.padre.networking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
